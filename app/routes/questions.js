f5050c00caa10f8dc92340969ddd586121e1cfeb
Tariq Mamdouh Ahmed <tarq.mamdouh1996@gmail.com>
2021-03-22 19:52:16 +0100

    QuestionsIndex: Moved Index to questions folder

--- app/routes/questions.js
+++ app/routes/questions.js
@@ -1,16 +1,4 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-    queryParams: {
-        page: {
-            refreshModel: true
-        },
-        tags: {
-            refreshModel: true
-        }
-    },
-
-    model: function(params) {
-        return this.store.query('question', params);
-    }
 });
