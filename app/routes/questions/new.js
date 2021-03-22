d792940f9ed214b1c19c678ebc531391de851b1b
Tariq Mamdouh Ahmed <tarq.mamdouh1996@gmail.com>
2021-03-22 23:54:38 +0100

    Questions: Added new Resource

--- app/routes/questions/new.js
+++ app/routes/questions/new.js
@@ -2,16 +2,20 @@ import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from "ember-simple-auth/mixins/authenticated-route-mixin";
 
 export default Route.extend(AuthenticatedRouteMixin, {
-    actions: {
+    model() {
+        return this.store.createRecord('question');
+      },
+    
+      actions: {
         save(record) {
-            record.save()
-                .then(() => this.transitionTo('questions'));
+          record.save()
+            .then(() => this.transitionTo('questions.show', record));
         },
-
+    
         willTransition() {
-            this._super(...arguments);
-            const record = this.controller.get('model');
-            record.rollbackAttributes();
+          this._super(...arguments);
+          const record = this.controller.get('model');
+          record.rollbackAttributes();
         },
-    },
+      },
 });
